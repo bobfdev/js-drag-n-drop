8e22c3ff435853b36b768d28112bb7148a177e11
Bob F <bobfdev@gmail.com>
2021-11-21 22:26:54 -0600

    added index.html and script.js files

---
+++ script.js
@@ -0,0 +1,44 @@
+const draggable_list = document.getElementById('draggable_list');
+const check = document.getElementById('check');
+
+const usCities = [
+    'Austin, TX',
+    'Phoenix, AZ',
+    'Miami, FL',
+    'Las Vegas, NV',
+    'Dallas, TX',
+    'Atlanta, GA',
+    'Charlotte, NC',
+    'Denver, CO',
+    'Charleston, SC',
+    'Tampa, FL',
+];
+
+// store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList(); 
+
+// Insert list items into DOM
+function createList() {
+    [...usCities]
+        .forEach((city, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+                <span class="number">${index + 1}</span>
+                <div class="draggable" draggable="true">
+                    <p class="us-city">${city}</p>
+                    <i class="fas fa-grip-lines"></i>
+                </div>
+            `;
+
+            listItems.push(listItem);
+
+            draggable_list.appendChild(listItem);
+        });
+}
